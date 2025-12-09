90dfc16160ea6db36477980b6655fe15c32a30de
PichuIsBest <maddoxrg@icloud.com>
2025-12-09 14:58:19 -0800

    feat: finished VisionIO and put placeholders in VisionConstants, some code finished in Vision

--- src/main/java/org/team1540/robot/subsystems/vision/apriltag/AprilTagVision.java
+++ src/main/java/org/team1540/robot/subsystems/vision/apriltag/AprilTagVision.java
@@ -1,4 +1,43 @@
 package org.team1540.robot.subsystems.vision.apriltag;
 
+import static org.team1540.robot.subsystems.vision.apriltag.AprilTagVisionConstants.*;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.littletonrobotics.junction.Logger;
+//import org.team1540.robot.FieldConstants;
+import org.team1540.robot.RobotState;
+import org.team1540.robot.subsystems.vision.apriltag.AprilTagVisionIO.PoseObservation;
+//import org.team1540.robot.util.LoggedTracer;
+
 public class AprilTagVision {
+
+//    private final AprilTagVisionIO[] visionIOs;
+//    private final AprilTagVisionIOInputsAutoLogged[] cameraInputs;
+//
+//    private final Alert[] disconnectedAlerts;
+//
+//    private final ArrayList<Pose3d> lastAcceptedPoses = new ArrayList<>();
+//    private final ArrayList<Pose3d> lastRejectedPoses = new ArrayList<>();
+//    private final ArrayList<Pose3d> lastSeenTagPoses = new ArrayList<>();
+//
+//    private AprilTagVision(AprilTagVisionIO... visionIOs) {
+//        this.visionIOs = visionIOs;
+//
+//        this.cameraInputs = new AprilTagVisionIOInputsAutoLogged[visionIOs.length];
+//        this.disconnectedAlerts = new Alert[visionIOs.length];
+//
+//        for (int count = 0; count < cameraInputs.length; count++) {
+//            cameraInputs[count] = new AprilTagVisionIOInputsAutoLogged();
+//            disconnectedAlerts[count] = new Alert(visionIOs[count].name + " is disconnected.", Alert.AlertType.kWarning);
+//        }
+//    }
+//
+//    public void periodic() {
+//        LoggedTracer.reset();
+//    }
+
 }
--- src/main/java/org/team1540/robot/subsystems/vision/apriltag/AprilTagVisionIOPhoton.java
+++ src/main/java/org/team1540/robot/subsystems/vision/apriltag/AprilTagVisionIOPhoton.java
@@ -1,4 +1,15 @@
 package org.team1540.robot.subsystems.vision.apriltag;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import java.util.*;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+//import org.team1540.robot.FieldConstants;
+
 public class AprilTagVisionIOPhoton {
 }
