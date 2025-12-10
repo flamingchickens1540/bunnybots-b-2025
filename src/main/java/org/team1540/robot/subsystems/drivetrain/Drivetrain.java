906bf481672bda1c584427a570d7bfa4eed7784d
UberDog711 <jamesrubyportland@gmail.com>
2025-12-09 17:34:08 -0800

    more stuff

--- src/main/java/org/team1540/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/org/team1540/robot/subsystems/drivetrain/Drivetrain.java
@@ -1,8 +1,16 @@
 package org.team1540.robot.subsystems.drivetrain;
 
 
+import edu.wpi.first.math.geometry.Rotation2d;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Drivetrain {
-    public static ReentrantLock odometryLock= new ReentrantLock();
+    public static ReentrantLock odometryLock = new ReentrantLock();
+
+    private final GyroIO gyroIO;
+    private final GyroIOInputsAutoLogged gyroInputs = new GyroIOInputsAutoLogged();
+    private final Module[] modules = new Module[4];
+
+    private Rotation2d fieldOrrientatedOffset = Rotation2d.kZero;
 }
