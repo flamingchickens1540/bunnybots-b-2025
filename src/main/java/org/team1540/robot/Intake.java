1d9aa6a170ba462503fb144a24ad72b6a85a4f69
sophia <salibas@catlin.edu>
2025-12-10 17:18:39 -0800

    feat: intake subsystem for robot b

---
+++ src/main/java/org/team1540/robot/Intake.java
@@ -0,0 +1,42 @@
+package org.team1540.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+    private static boolean hasInstance = false;
+
+    private final IntakeIO io;
+
+    private Intake(IntakeIO io) {
+        if (hasInstance) throw new IllegalStateException("Instance of intake already exists");
+        hasInstance = true;
+        this.io = io;
+    }
+
+    public void periodic() {
+        // FIGURE THIS OUT
+        //LoggedTracer.reset();
+
+        //io.updateInputs(inputs);
+        //Logger.processInputs("Intake", inputs);
+
+        if (DriverStation.isDisabled()) stopAll();
+
+        //LoggedTracer.record("Intake");
+    }
+
+    public void setIntakeVoltage(double voltage) {
+        io.setIntakeVoltage(voltage);
+    }
+
+    public void stopAll() {
+        setIntakeVoltage(0);
+    }
+
+    public Command commandRunIntake(double percent) {
+        return Commands.startEnd(() -> this.setIntakeVoltage(percent * 12), () -> this.setIntakeVoltage(0), this);
+    }
+}
---
+++ src/main/java/org/team1540/robot/IntakeIO.java
@@ -0,0 +1,17 @@
+package org.team1540.robot;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+    @AutoLog
+    class IntakeInputs {
+        public double intakeMotorVelocityRPS = 0;
+        public double intakeMotorAppliedVolts = 0;
+        public double intakeSupplyCurrentAmps = 0;
+        public double intakeStatorCurrentAmps = 0;
+    }
+
+    default void setIntakeVoltage(double voltage) {}
+
+    default void updateInputs(IntakeInputs inputs) {}
+}
